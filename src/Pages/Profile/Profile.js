9f4e0e58a01473e6244d22a78b2d9d5f4835609c
akaash67 <md.akash6724@gmail.com>
2021-12-19 18:45:16 +0600

    done

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-            this is my profile
+          <h1 className="text-center">This page will be updated soon</h1>
         </div>
     );
 };
